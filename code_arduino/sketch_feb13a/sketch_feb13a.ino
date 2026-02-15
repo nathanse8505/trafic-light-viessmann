ea5bc47f745cbe250d3b3a688ee952a2251c2677
Nathan Sebag <nathans@mada.org.il>
2026-02-15 15:23:53 +0200

    new version

--- code_arduino/sketch_feb13a/sketch_feb13a.ino
+++ code_arduino/sketch_feb13a/sketch_feb13a.ino
@@ -1,20 +1,36 @@
-#include <Wire.h>
-#include <Adafruit_PWMServoDriver.h>
+#define CROSSWALK_IO_E1_E2 2 
+#define TRAFFIC_G_IO_A 3
+#define TRAFFIC_Y_IO_A 4
+#define TRAFFIC_R_IO_A 5
+
+#define TRAFFIC_G_IO_B 6
+#define TRAFFIC_Y_IO_B 7
+#define TRAFFIC_R_IO_B 8
+
+#define TRAFFIC_G_IO_C 9
+#define TRAFFIC_Y_IO_C 10
+#define TRAFFIC_R_IO_C 11
+
+#define TRAFFIC_G_IO_D 12
+#define TRAFFIC_Y_IO_D A0
+#define TRAFFIC_R_IO_D A1
+
+#define PED_G_IO_A3_A4 A2
+#define PED_R_IO_A3_A4 A3
+
+#define PED_G_IO_B3_B4 A4
+#define PED_R_IO_B3_B4 A5
 
 
-const uint8_t TRAFFIC_LIGHT_A[3] = {0,1,2};// pedestrian A1 connected to pedestrian A2
-const uint8_t TRAFFIC_LIGHT_B[3] = {3,4,5}; //pedestrian B1 connected to pedestrian B2
-const uint8_t TRAFFIC_LIGHT_C[3] = {6,7,8}; // pedestrian C1 connected to pedestrian C2
-const uint8_t TRAFFIC_LIGHT_D[3] = {9,10,11};// pedestrian D1 connected to pedestrian D2
-const uint8_t PEDESTRIANS_A3_A4[2] = {12,13};
-const uint8_t PEDESTRIANS_B3_B4[2] = {14,15};
-
-#define CROSSWALK_IO_E1 3 
-#define CROSSWALK_IO_E2 4 
+const uint8_t TRAFFIC_LIGHT_A[3] = {TRAFFIC_G_IO_A,TRAFFIC_Y_IO_A,TRAFFIC_R_IO_A};// pedestrian A1 connected to pedestrian A2
+const uint8_t TRAFFIC_LIGHT_B[3] = {TRAFFIC_G_IO_B,TRAFFIC_Y_IO_B,TRAFFIC_R_IO_B};// pedestrian A1 connected to pedestrian A2
+const uint8_t TRAFFIC_LIGHT_C[3] = {TRAFFIC_G_IO_C,TRAFFIC_Y_IO_C,TRAFFIC_R_IO_C};// pedestrian A1 connected to pedestrian A2
+const uint8_t TRAFFIC_LIGHT_D[3] = {TRAFFIC_G_IO_D,TRAFFIC_Y_IO_D,TRAFFIC_R_IO_D};// pedestrian A1 connected to pedestrian A2
+const uint8_t PEDESTRIANS_A3_A4[2] = {PED_G_IO_A3_A4,PED_R_IO_A3_A4};
+const uint8_t PEDESTRIANS_B3_B4[2] = {PED_G_IO_B3_B4,PED_R_IO_B3_B4};
 
 
-// Adresse I2C du PCA9685 (souvent 0x40)
-Adafruit_PWMServoDriver pca = Adafruit_PWMServoDriver(0x40);
+
 
 
 const uint16_t PWM_MAX = 4095;     // 12-bit: 0..4095
@@ -29,11 +45,9 @@ const uint8_t RED_WIRE_PED = 1;
 int8_t seq = 1;
 long timer_btw_sq = 0;
 const uint32_t TIME_SEQ = 5000;
-const uint32_t TIME_BTW_SEQ = 1500;
+const uint32_t TIME_BTW_SEQ = 1000;
 
-const int8_t NUMBER_OF_CROSSWALK = 2;
-uint8_t CROSSWALKS[NUMBER_OF_CROSSWALK] = {CROSSWALK_IO_E1,CROSSWALK_IO_E2};
-const unsigned long CROSSWALK_DELAY = 1500;
+const unsigned long CROSSWALK_DELAY = 1000;
 unsigned long crosswalk_timer = 0;
 bool crosswalk_state = false; // false=LOW, true=HIGH
 
@@ -41,80 +55,88 @@ bool crosswalk_state = false; // false=LOW, true=HIGH
 void INIT_CROSSWALK(){
   crosswalk_timer = millis();
   crosswalk_state = false;
-  for (uint8_t i = 0; i < NUMBER_OF_CROSSWALK; i++) digitalWrite(CROSSWALKS[i], LOW);
+  digitalWrite(CROSSWALK_IO_E1_E2, LOW);
+}
+void INIT_G_pinmode(uint8_t* ch_traffic_light){
+  pinMode(ch_traffic_light[GREEN_WIRE],OUTPUT);
+  pinMode(ch_traffic_light[YELLOW_WIRE],OUTPUT);
+  pinMode(ch_traffic_light[RED_WIRE],INPUT);
 }
 
-void BLINK_CROSSWALK(const uint8_t *crosswalk_io, uint8_t count) {
+void INIT_Y_pinmode(uint8_t* ch_traffic_light){
+  pinMode(ch_traffic_light[GREEN_WIRE],INPUT);
+  pinMode(ch_traffic_light[YELLOW_WIRE],OUTPUT);
+  pinMode(ch_traffic_light[RED_WIRE],OUTPUT);
+}
 
-  if (millis(); - crosswalk_timer >= CROSSWALK_DELAY) {
+void INIT_R_pinmode(uint8_t* ch_traffic_light){
+  pinMode(ch_traffic_light[GREEN_WIRE],INPUT);
+  pinMode(ch_traffic_light[YELLOW_WIRE],OUTPUT);
+  pinMode(ch_traffic_light[RED_WIRE],OUTPUT);
+}
+
+void INIT_PED_pinmode(uint8_t* ch_traffic_light){
+  pinMode(ch_traffic_light[GREEN_WIRE],OUTPUT);
+  pinMode(ch_traffic_light[YELLOW_WIRE],INPUT);
+  pinMode(ch_traffic_light[RED_WIRE],OUTPUT);
+}
+
+
+
+void BLINK_CROSSWALK(const uint8_t *crosswalk_io) {
+
+  if (millis() - crosswalk_timer >= CROSSWALK_DELAY) {
     crosswalk_timer = millis();
     crosswalk_state = !crosswalk_state;
-
-    for (uint8_t i = 0; i < count; i++) {
-      digitalWrite(crosswalk_io[i], crosswalk_state ? HIGH : LOW);
-    }
+    digitalWrite(CROSSWALK_IO_E1_E2, crosswalk_state ? HIGH : LOW);
   }
 }
 
 
 
-void RED_LIGHT_GREEN_PEDESTRIAN(uint8_t* ch_traffic_light){
-   pca.setPWM(ch_traffic_light[GREEN_WIRE], 0, 0);   // ON à 0, OFF à v => duty proportionnel
-   pca.setPWM(ch_traffic_light[YELLOW_WIRE], 0, 0);   // ON à 0, OFF à v => duty proportionnel
-   pca.setPWM(ch_traffic_light[RED_WIRE], 0,  PWM_MAX);   // ON à 0, OFF à v => duty proportionnel
-
-}
-
-void RED_LIGHT_RED_PEDESTRIAN(uint8_t* ch_traffic_light){
-   pca.setPWM(ch_traffic_light[GREEN_WIRE], 0, 0);   // ON à 0, OFF à v => duty proportionnel
-   pca.setPWM(ch_traffic_light[YELLOW_WIRE], 0, 0);   // ON à 0, OFF à v => duty proportionnel
-   pca.setPWM(ch_traffic_light[RED_WIRE], 0,  PWM_MAX);   // ON à 0, OFF à v => duty proportionnel
-
-}
-
-void YELLOW_LIGHT_RED_PEDESTRIAN(uint8_t* ch_traffic_light){
-   pca.setPWM(ch_traffic_light[GREEN_WIRE], 0,  0);   // ON à 0, OFF à v => duty proportionnel
-   pca.setPWM(ch_traffic_light[YELLOW_WIRE], 0, PWM_MAX);   // ON à 0, OFF à v => duty proportionnel
-   pca.setPWM(ch_traffic_light[RED_WIRE], 0, 0);   // ON à 0, OFF à v => duty proportionnel
-  
+void RED_LIGHT(uint8_t* ch_traffic_light){
+  INIT_R_pinmode(ch_traffic_light);
+  digitalWrite(ch_traffic_light[YELLOW_WIRE], LOW);
+  digitalWrite(ch_traffic_light[RED_WIRE], HIGH);
+   
 }
 
 void YELLOW_LIGHT(uint8_t* ch_traffic_light){
-   pca.setPWM(ch_traffic_light[GREEN_WIRE], 0, 0);   // ON à 0, OFF à v => duty proportionnel
-   pca.setPWM(ch_traffic_light[YELLOW_WIRE], 0, PWM_MAX);   // ON à 0, OFF à v => duty proportionnel
-   pca.setPWM(ch_traffic_light[RED_WIRE], 0, 0);   // ON à 0, OFF à v => duty proportionnel
-  
+  INIT_Y_pinmode(ch_traffic_light);
+  digitalWrite(ch_traffic_light[GREEN_WIRE], LOW);
+  digitalWrite(ch_traffic_light[YELLOW_WIRE], HIGH);
+  digitalWrite(ch_traffic_light[RED_WIRE], LOW);
+   
 }
 
-
-void GREEN_LIGHT_GREEN_PEDESTRIAN(uint8_t* ch_traffic_light){
-  pca.setPWM(ch_traffic_light[GREEN_WIRE], 0, PWM_MAX);   // ON à 0, OFF à v => duty proportionnel
-  pca.setPWM(ch_traffic_light[YELLOW_WIRE], 0,0);   // ON à 0, OFF à v => duty proportionnel
-  pca.setPWM(ch_traffic_light[RED_WIRE], 0, 0);   // ON à 0, OFF à v => duty proportionnel
+void GREEN_LIGHT(uint8_t* ch_traffic_light){
+  INIT_G_pinmode(ch_traffic_light);
+  digitalWrite(ch_traffic_light[GREEN_WIRE], HIGH);
+  digitalWrite(ch_traffic_light[YELLOW_WIRE], LOW);
 }
 
-void GREEN_LIGHT_RED_PEDESTRIAN(uint8_t* ch_traffic_light){
-  pca.setPWM(ch_traffic_light[GREEN_WIRE], 0, PWM_MAX);   // ON à 0, OFF à v => duty proportionnel
-  pca.setPWM(ch_traffic_light[YELLOW_WIRE], 0, 0);   // ON à 0, OFF à v => duty proportionnel
-  pca.setPWM(ch_traffic_light[RED_WIRE], 0, 0);   // ON à 0, OFF à v => duty proportionnel
+void RED_LIGHT_PED(uint8_t* ch_traffic_light){
+  INIT_PED_pinmode(ch_traffic_light);
+  digitalWrite(ch_traffic_light[GREEN_WIRE], LOW);
+  digitalWrite(ch_traffic_light[RED_WIRE], HIGH);
 }
 
-
-void BLANK_LIGHT(uint8_t* ch_traffic_light){
-  pca.setPWM(ch_traffic_light[GREEN_WIRE], 0, 0);   // ON à 0, OFF à v => duty proportionnel
-  pca.setPWM(ch_traffic_light[YELLOW_WIRE], 0, 0);   // ON à 0, OFF à v => duty proportionnel
-  pca.setPWM(ch_traffic_light[RED_WIRE], 0, 0);   // ON à 0, OFF à v => duty proportionnel
+void GREEN_LIGHT_PED(uint8_t* ch_traffic_light){
+  INIT_PED_pinmode(ch_traffic_light);
+  digitalWrite(ch_traffic_light[GREEN_WIRE], HIGH);
+  digitalWrite(ch_traffic_light[RED_WIRE], LOW);
 }
 
-void GREEN_LIGHT_PEDESTRIAN(uint8_t* ch_pedestrian){
-  pca.setPWM(ch_pedestrian[GREEN_WIRE_PED], 0, PWM_MAX);   // ON à 0, OFF à v => duty proportionnel
-  pca.setPWM(ch_pedestrian[RED_WIRE_PED], 0, 0);   // ON à 0, OFF à v => duty proportionnel
+void RED_LIGHT_PED_O(uint8_t* ch_traffic_light){
+  digitalWrite(ch_traffic_light[GREEN_WIRE], LOW);
+  digitalWrite(ch_traffic_light[RED_WIRE], HIGH);
 }
 
-void RED_LIGHT_PEDESTRIAN(uint8_t* ch_pedestrian){
-  pca.setPWM(ch_pedestrian[GREEN_WIRE_PED], 0, 0);   // ON à 0, OFF à v => duty proportionnel
-  pca.setPWM(ch_pedestrian[RED_WIRE_PED], 0, PWM_MAX);   // ON à 0, OFF à v => duty proportionnel
+void GREEN_LIGHT_PED_O(uint8_t* ch_traffic_light){
+  digitalWrite(ch_traffic_light[GREEN_WIRE], HIGH);
+  digitalWrite(ch_traffic_light[RED_WIRE], LOW);
 }
+/*
 
 
 
@@ -141,28 +163,56 @@ void SEQ_1_2(){
    YELLOW_LIGHT(TRAFFIC_LIGHT_C);
 
 }
-
+*/
 
 void setup() {
-  pinMode(CROSSWALK_IO_E1,OUTPUT);
-  pinMode( CROSSWALK_IO_E2,OUTPUT);
+  Serial.begin(115200);
+  pinMode(CROSSWALK_IO_E1_E2,OUTPUT);
 
-  Wire.begin();
-  pca.begin();
-  // Fréquence PWM (LED : 500-1000 Hz marche bien)
-  pca.setPWMFreq(1000);
+  pinMode(TRAFFIC_G_IO_A,OUTPUT);
+  pinMode(TRAFFIC_Y_IO_A,OUTPUT);
+  pinMode(TRAFFIC_R_IO_A,OUTPUT);
 
-  // LED éteinte au départ
-  for(int i = 0;i < NUMBER_OF_OUTPUT;i++){
-      pca.setPWM(i, 0, 0);
-  }
+  pinMode(TRAFFIC_G_IO_B,OUTPUT);
+  pinMode(TRAFFIC_Y_IO_B,OUTPUT);
+  pinMode(TRAFFIC_R_IO_B,OUTPUT);
+
+  pinMode(TRAFFIC_G_IO_C,OUTPUT);
+  pinMode(TRAFFIC_Y_IO_C,OUTPUT);
+  pinMode(TRAFFIC_R_IO_C,OUTPUT);
+
+  pinMode(TRAFFIC_G_IO_D,OUTPUT);
+  pinMode(TRAFFIC_Y_IO_D,OUTPUT);
+  pinMode(TRAFFIC_R_IO_D,OUTPUT);
+
+  pinMode(PED_G_IO_A3_A4,OUTPUT);
+  pinMode(PED_R_IO_A3_A4,OUTPUT);
+
+  pinMode(PED_G_IO_B3_B4,OUTPUT);
+  pinMode(PED_R_IO_B3_B4,OUTPUT);
+
   INIT_CROSSWALK();
+  Serial.println("init");
   
 }
 
 
 void loop() {
-  BLINK_CROSSWALK(CROSSWALKS, NUMBER_OF_CROSSWALK);
+  BLINK_CROSSWALK(CROSSWALK_IO_E1_E2);
+  //RED_LIGHT(TRAFFIC_LIGHT_A);
+  //delay(5);
+  //YELLOW_LIGHT(TRAFFIC_LIGHT_A);
+  //delay(5);
+  //GREEN_LIGHT_PED(TRAFFIC_LIGHT_A);
+
+  RED_LIGHT(TRAFFIC_LIGHT_B);
+  delay(5);
+  YELLOW_LIGHT(TRAFFIC_LIGHT_B);
+  delay(5);
+  GREEN_LIGHT_PED(TRAFFIC_LIGHT_B);
+  delay(5);
+ 
+  /*
   if(seq == 1 && (millis() - timer_btw_sq >= TIME_SEQ)){
     SEQ_1_1();
     seq++;
